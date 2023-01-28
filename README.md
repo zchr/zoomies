b1ba5aa7f769a31acfc3cca60928beed1706d91d
Zach Schnell <zach.schnell@angel.co>
2023-01-27 23:27:47 -0500

    Add additional sites & limit host_permissions, prep for v0.6.0

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # What / why
+
 An extension to auto-close tabs after they open native apps. Useful for sites like Zoom & Notion that leave stale tabs open after their desktop app has launched.
 
- - [Available as Chrome extension](https://chrome.google.com/webstore/detail/goodnight-tabs/paichadkkbhdmkngdmkgmefiabjjcaai)
- - Firefox extension TBD ([GH issue](https://github.com/zchr/zoomies/issues/1))
- 
- ![Screenshot of extension](https://lh3.googleusercontent.com/crv9VN20_Qj1s2o86GUwE5n-cgnupK3KLhWk6WiRoznxFhT1mb4sLAUZ9z5MXBJk9NEaR6x-Zg62oBrGUeVEyfNAvko=w640-h400-e365-rj-sc0x00ffffff)
+- [Available as Chrome extension](https://chrome.google.com/webstore/detail/goodnight-tabs/paichadkkbhdmkngdmkgmefiabjjcaai)
+- Firefox extension TBD ([GH issue](https://github.com/zchr/zoomies/issues/1))
 
-# Changes
-Feel free to [create an issue](https://github.com/zchr/zoomies/issues/new) with any other sites you'd want the extension to support. Please include:
- - Url of the service
- - A screenshot of the tab that you'd want closed (this way, we know what text / patterns on the page to match on)
+![Screenshot of extension](https://lh3.googleusercontent.com/crv9VN20_Qj1s2o86GUwE5n-cgnupK3KLhWk6WiRoznxFhT1mb4sLAUZ9z5MXBJk9NEaR6x-Zg62oBrGUeVEyfNAvko=w640-h400-e365-rj-sc0x00ffffff)
+
+# Currently works on
+
+- Around ([around.co](https://around.co))
+- Asana ([asana.com](https://asana.com))
+- AWS ([awsapps.com](https://awsapps.com))
+- Figma ([figma.com](https://figma.com))
+- Miro ([miro.com](https://miro.com))
+- Notion ([notion.so](https://notion.so))
+- Teams ([teams.live.com](https://teams.live.com))
+- Zoom ([zoom.us](https://zoom.us))
+
+# Changes / suggestions?
+
+Feel free to [create an issue](https://github.com/zchr/zoomies/issues/new) with any suggestions. If there are other sites you'd want the extension to support, Please include:
+
+- Url of the service
+- A screenshot of the tab that you'd want closed (this way, we know what text / patterns on the page to match on)
+
+## Adding a new services
+
+Requires updates the following places:
+
+1. Add new checkbox in `script.js`
+2. Add new match function in `background.js`, and include function in the `checks` array
+3. Add the domain to `manifest.json`
+4. Add the domain to `README.md` (^^)
--- background.js
+++ background.js
@@ -4,12 +4,38 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 
   async function execute() {
+    function isAroundPage() {
+      return isMatch("around", "around.co", ["Continue in Browser"]);
+    }
+
     function isAsanaPage() {
       return isMatch("asana", "asana.com", ["Opening link in Asana"]);
     }
 
+    function isAWSPage() {
+      return isMatch("aws", "awsapps.com", ["You may now close this browser"]);
+    }
+
+    function isFigmaPage() {
+      return isMatch("figma", "figma.com", ["Open here instead"]);
+    }
+
+    function isLinearPage() {
+      return isMatch("linear", "linear.app", [
+        "The link was opened in the Linear app",
+      ]);
+    }
+
+    function isMiroPage() {
+      return isMatch("miro", "miro.com/app/board/", null);
+    }
+
     function isNotionPage() {
       return isMatch("notion", "notion.so", ["Redirecting to your Notion app"]);
+    }
+
+    function isTeamsPage() {
+      return isMatch("teams", "teams.live.com", ["Open your Teams app"]);
     }
 
     function isZoomPage() {
@@ -20,26 +46,37 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 
     async function isMatch(name, origin, searches) {
-      const regex = new RegExp(`https://[a-z]*\\.${origin}`);
+      const regex = new RegExp(`https://([a-z]*\\.)?${origin}`, "g");
       const isSiteActive = (await chrome.storage.local.get("sites")).sites[
         name
       ];
 
       return (
         isSiteActive &&
-        window.location.origin.match(regex) &&
-        searches.find((search) =>
-          window.find(
-            search,
-            /* aCaseSensitive */ false,
-            /* aBackwards */ false,
-            /* aWrapAround */ true
-          )
-        )
+        window.location.href.match(regex) &&
+        (searches == null ||
+          searches.find((search) =>
+            window.find(
+              search,
+              /* aCaseSensitive */ false,
+              /* aBackwards */ false,
+              /* aWrapAround */ true
+            )
+          ))
       );
     }
 
-    const checks = [isAsanaPage, isNotionPage, isZoomPage];
+    const checks = [
+      isAroundPage,
+      isAsanaPage,
+      isAWSPage,
+      isFigmaPage,
+      isLinearPage,
+      isMiroPage,
+      isNotionPage,
+      isTeamsPage,
+      isZoomPage,
+    ];
 
     for (let i = 0; i < checks.length; i++) {
       if (await checks[i]()) {
